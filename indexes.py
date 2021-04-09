66c443caf0a741ea589ec82d6b6e2a7ab3c2bccb
Wesley Ameixoeira <wesley.ameixoeira@ibm.com>
2021-04-09 18:08:25 -0300

    first commit

---
+++ indexes.py
@@ -0,0 +1,23 @@
+def indexes(a):
+    
+    number = int(input("enter the number you want to search for: "))
+    for i in list:
+        print(enumerate(list.index(number))
+
+
+#defines the list
+list = []
+
+#asks for a given amount of elements
+n = int(input("Enter number of elements : "))
+
+#loops until we have the desired ammount
+for i in range (0,n):
+    elements = int(input("Input an element :"))
+    list.append(elements)
+
+#print(list.index(5))
+indexes(list)
+
+
+
