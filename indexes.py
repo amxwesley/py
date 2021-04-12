5a6c284f18542779aac09559545aaaf0688608e9
Wesley Ameixoeira <wesley.ameixoeira@ibm.com>
2021-04-12 11:37:12 -0300

    remodel2

--- indexes.py
+++ indexes.py
@@ -1,7 +1,8 @@
+ints = [1, 3, 3, 5, 4, 3]
+
 def indexes(item):
-    ints = [1, 3, 3, 5, 4, 3]
     
     indexes = [i for i in range(len(ints)) if ints[i] == item]
     print(f"Item {item} is found at index {indexes}")
 
-indexes(4)
+indexes(3)
