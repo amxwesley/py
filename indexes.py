6f8459b851b3190b51d286ef6ea10e35fb33dc00
Wesley Ameixoeira <45472694+amxwesley@users.noreply.github.com>
2021-04-13 11:59:46 -0300

    Update indexes.py

--- indexes.py
+++ indexes.py
@@ -3,6 +3,6 @@ ints = [1, 3, 3, 5, 4, 3]
 def indexes(item):
     
     indexes = [i for i in range(len(ints)) if ints[i] == item]
-    print(f"Item {item} is found at index {indexes}")
+    print("Item {item} is found at index {indexes}")
 
 indexes(3)
